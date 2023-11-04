fdf857d98991cdd3e247a3bf6352ababfb8ceb78
CringleSD <100538275+CringleySDays@users.noreply.github.com>
2023-11-04 23:39:38 +0000

    Create .bash

---
+++ .bash
@@ -0,0 +1,15 @@
+developmentType=$1
+red="\033[0;31m"
+green="\033[0;32m"
+colourless="\033[0m"
+
+if [ $developmentType = "dev" ]; then
+    yarn run dev:install && yarn run dev
+
+elif [ $developmentType = "prod" ]; then
+    yarn run prod:install && yarn run build
+    rm -r src, .prettierrc.js, tsconfig.json
+    NODE_ENV=prod yarn run start
+else 
+    echo -e "${red}Invalid. ${colourless}Please use ${green}dev ${colourless} or ${green}prod."
+fi
