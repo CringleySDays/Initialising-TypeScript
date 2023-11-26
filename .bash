3a892fcdc6d5c5caabf4528af31ee7aa925974bb
CringleSD <100538275+CringleySDays@users.noreply.github.com>
2023-11-26 16:37:37 +0000

    Update .bash

--- .bash
+++ .bash
@@ -4,10 +4,10 @@ green="\033[0;32m"
 colourless="\033[0m"
 
 if [ $developmentType = "dev" ]; then
-    yarn run dev:install && yarn run dev
+    yarn run dev:install && yarn run dev:upgrade && yarn run dev
 
 elif [ $developmentType = "prod" ]; then
-    yarn run prod:install && yarn run build
+    yarn run prod:install && yarn run prod:upgrade && yarn run build
     rm -r src, .prettierrc.js, tsconfig.json
     NODE_ENV=prod yarn run start
 else 
